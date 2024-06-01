692cec81c44a74204e6d1dfb0da6f254f32be3e8
Oceanity <github@oceanity.net>
2024-06-01 11:57:11 -0400

    Add replaceVariableManager

--- src/utils/firebot.ts
+++ src/utils/firebot.ts
@@ -4,6 +4,7 @@ import { ScriptModules } from "@crowbartools/firebot-custom-scripts-types";
 export let logger: ScriptModules["logger"];
 export let effectRunner: ScriptModules["effectRunner"];
 export let effectManager: ScriptModules["effectManager"];
+export let replaceVariableManager: ScriptModules["replaceVariableManager"];
 export let integrationManager: ScriptModules["integrationManager"];
 export let jsonDb: ScriptModules["JsonDb"];
 export let utils: ScriptModules["utils"];
@@ -12,6 +13,7 @@ export function initModules(scriptModules: ScriptModules) {
   logger = scriptModules.logger;
   effectRunner = scriptModules.effectRunner;
   effectManager = scriptModules.effectManager;
+  replaceVariableManager = scriptModules.replaceVariableManager;
   integrationManager = scriptModules.integrationManager;
   jsonDb = scriptModules.JsonDb;
   utils = scriptModules.utils;
