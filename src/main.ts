8c9bd9f58572df30f7b591513c124be54d57fa03
Izzy Deane <github@oceanity.net>
2024-06-03 10:49:24 -0400

    Bumped Version

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ const script: Firebot.CustomScript<Params> = {
       name: "Firebot Spotify Integrations",
       description: "Let your viewers determine your taste in music",
       author: "Oceanity",
-      version: "0.6.2",
+      version: "0.6.3",
       firebotVersion: "5",
     };
   },
