4936ea01f1de1f773539e8655c94bf949a83d92f
Oceanity <github@oceanity.net>
2024-06-16 16:30:59 -0400

    More Playlist Vars (#47)
    * Added new variables to Playlist, updated test to use more reliable property
    
    * Fixed other param that could be simplified
    
    * Fixed incorrect return
    
    * Forgot those were API links, oops
    
    * Updated Tests

---
+++ src/firebot/variables/playlist/spotifyPlaylistOwnerUrl.ts
@@ -0,0 +1,13 @@
+import { spotify } from "@/main";
+import { ReplaceVariable } from "@crowbartools/firebot-custom-scripts-types/types/modules/replace-variable-manager";
+
+export const SpotifyPlaylistOwnerUrlVariable: ReplaceVariable = {
+  definition: {
+    handle: "spotifyPlaylistOwnerUrl",
+    description:
+      "Gets the Spotify Profile URL to the Owner of the currently playing Spotify Playlist",
+    usage: "spotifyPlaylistOwnerUrl",
+    possibleDataOutput: ["text"],
+  },
+  evaluator: async () => spotify.player.playlist.ownerUrl,
+};
