c54a833b688d3acfe4e6f92916b8156206cee6f2
Izzy Deane <github@oceanity.net>
2024-06-19 16:42:25 -0400

    Removed duplicate emit

--- src/tamperMonkey/lyricsGrabber.user.js
+++ src/tamperMonkey/lyricsGrabber.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Lyric Grabber
 // @namespace    https://oceanity.github.io
-// @version      0.7.1
+// @version      0.7.1a
 // @description  Sends Spotify lyrics to Firebot
 // @author       Oceanity
 // @match        https://open.spotify.com/*
