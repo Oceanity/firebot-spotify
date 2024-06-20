1686412cb7ffd412bf250987410921d399af3e39
Izzy Deane <github@oceanity.net>
2024-06-19 20:23:00 -0400

    Deprecated spotifyTrackArtist

--- src/firebot/variables/track/spotifyTrackArtist.ts
+++ src/firebot/variables/track/spotifyTrackArtist.ts
@@ -5,9 +5,9 @@ export const SpotifyTrackArtistVariable: ReplaceVariable = {
   definition: {
     handle: "spotifyTrackArtist",
     description:
-      "Gets the primary artist of the currently playing track on Spotify or empty string if not playing",
+      "DEPRECATED, use $spotifyTrack[artist]. Gets the primary artist of the currently playing track on Spotify or empty string if not playing",
     usage: "spotifyTrackArtist",
     possibleDataOutput: ["text"],
   },
-  evaluator: async () => spotify.player.track.artist,
+  evaluator: async () => spotify.player.track.summary?.artist ?? "",
 };
