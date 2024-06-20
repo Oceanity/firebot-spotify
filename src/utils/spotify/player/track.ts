5bf4194fa74c37bfe86a8691ff15af4aefb275d1
Izzy Deane <github@oceanity.net>
2024-06-19 20:25:01 -0400

    Added summary to track

--- src/utils/spotify/player/track.ts
+++ src/utils/spotify/player/track.ts
@@ -1,14 +1,30 @@
 import { formatMsToTimecode } from "@/utils/string";
 import { getBiggestImageUrl } from "@utils/array";
-import { eventManager, logger } from "@utils/firebot";
 import { SpotifyService } from "@utils/spotify";
 import { EventEmitter } from "events";
+
+export type SpotifyTrackSummary = {
+  title: string;
+  artist: string;
+  artists: string[];
+  album: string;
+  albumArtUrl: string;
+  durationMs: number;
+  duration: string;
+};
+
+export type SpotifyTrackSummaryWithPosition = SpotifyTrackSummary & {
+  positionMs: number;
+  position: string;
+  relativePosition: number;
+};
 
 export class SpotifyTrackService extends EventEmitter {
   private readonly spotify: SpotifyService;
 
   private _track?: SpotifyTrackDetails;
-  private _progressMs?: number;
+  private _trackSummary?: SpotifyTrackSummary;
+  private _progressMs: number = 0;
 
   constructor(spotifyService: SpotifyService) {
     super();
@@ -34,6 +50,17 @@ export class SpotifyTrackService extends EventEmitter {
   //#region Getters
   public get raw(): SpotifyTrackDetails | null {
     return this._track ?? null;
+  }
+
+  public get summary(): SpotifyTrackSummaryWithPosition | null {
+    return this._trackSummary
+      ? {
+          ...this._trackSummary,
+          positionMs: this._progressMs,
+          position: formatMsToTimecode(this._progressMs),
+          relativePosition: this._progressMs / this._trackSummary.durationMs,
+        }
+      : null;
   }
 
   public get isTrackLoaded(): boolean {
@@ -98,10 +125,27 @@ export class SpotifyTrackService extends EventEmitter {
   public update(track?: SpotifyTrackDetails): void {
     if (this._track?.uri != track?.uri) {
       this._track = track;
+      this._trackSummary = trackSummaryFromDetails(track) ?? undefined;
     }
   }
 
   public async handleNextTick(progressMs?: number) {
-    this._progressMs = progressMs;
+    this._progressMs = progressMs ?? 0;
   }
+}
+
+export function trackSummaryFromDetails(
+  track?: SpotifyTrackDetails
+): SpotifyTrackSummary | null {
+  if (!track) return null;
+
+  return Object.freeze({
+    title: track.name,
+    artist: track.artists[0].name,
+    artists: track.artists.map((a) => a.name),
+    album: track.album.name,
+    albumArtUrl: getBiggestImageUrl(track.album.images),
+    durationMs: track.duration_ms,
+    duration: formatMsToTimecode(track.duration_ms),
+  });
 }
