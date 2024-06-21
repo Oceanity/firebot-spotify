3f78d8f3141b2497786d090f093957914c2cdc2f
Izzy Deane <github@oceanity.net>
2024-06-21 19:35:26 -0400

    Fixed typos in Deprecated message

--- src/firebot/variables/track/spotifyTrackUrl.ts
+++ src/firebot/variables/track/spotifyTrackUrl.ts
@@ -5,7 +5,7 @@ export const SpotifyTrackUrlVariable: ReplaceVariable = {
   definition: {
     handle: "spotifyTrackUrl",
     description:
-      "DEPRECATED, use $spotifyTrack[trackUrl]. Gets the shareable Url of the currently playing track on Spotify or empty string if not playing",
+      "DEPRECATED, use $spotifyTrack[url]. Gets the shareable Url of the currently playing track on Spotify or empty string if not playing",
     usage: "spotifyTrackUrl",
     possibleDataOutput: ["text"],
   },
