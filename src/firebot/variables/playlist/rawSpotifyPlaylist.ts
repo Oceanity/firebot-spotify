95ba21b69f1ed488da4e42a3107df8a0006d4193
Izzy Deane <github@oceanity.net>
2024-06-21 21:15:37 -0400

    Added information to Raw variables

--- src/firebot/variables/playlist/rawSpotifyPlaylist.ts
+++ src/firebot/variables/playlist/rawSpotifyPlaylist.ts
@@ -6,7 +6,7 @@ export const RawSpotifyPlaylistVariable: ReplaceVariable = {
   definition: {
     handle: "rawSpotifyPlaylist",
     description:
-      "Gets an object containing all data returned from the Spotify API about the currently playing playlist",
+      "Gets an object containing all data returned from the Spotify API about the currently playing playlist, see [Response Sample](https://developer.spotify.com/documentation/web-api/reference/get-playlist) for detail",
     usage: "rawSpotifyPlaylist",
     //@ts-expect-error ts2322
     possibleDataOutput: [OutputDataType.BOOLEAN],
--- src/firebot/variables/track/rawSpotifyTrack.ts
+++ src/firebot/variables/track/rawSpotifyTrack.ts
@@ -5,7 +5,7 @@ export const RawSpotifyTrackVariable: ReplaceVariable = {
   definition: {
     handle: "rawSpotifyTrack",
     description:
-      "Gets an object containing all data returned from the Spotify API about the currently playing track",
+      "Gets an object containing all data returned from the Spotify API about the currently playing track, see [Response Sample](https://developer.spotify.com/documentation/web-api/reference/get-track) for details",
     usage: "rawSpotifyTrack",
     possibleDataOutput: ["text"],
   },
